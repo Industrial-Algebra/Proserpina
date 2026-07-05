f52d31bcf4fce260d3c8461501cf65381cd0509e
Justin Elliott Cobb <justincobb76@pm.me>
2026-07-05 13:09:51 -0400

    fmt

--- examples/panel_smoke.rs
+++ examples/panel_smoke.rs
@@ -32,7 +32,8 @@ fn main() {
         None,
         false,         // markdown (not json)
         Some("panel"), // 5-critic panel
-        RetryPolicy::DEFAULT, None,
+        RetryPolicy::DEFAULT,
+        None,
     )
     .expect("run should succeed");
 
